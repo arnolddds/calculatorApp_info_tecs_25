3698fb855ed64aaf0ffa93020939cc725bf4a1c2
sobol <sobolev.arnold@mail.ru>
2025-07-24 00:16:39 +0700

    Initial commit

---
+++ app/src/main/java/com/sobolev/calculatorapp_info_tecs_25/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sobolev.calculatorapp_info_tecs_25
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.sobolev.calculatorapp_info_tecs_25.ui.theme.CalculatorApp_info_tecs_25Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorApp_info_tecs_25Theme {
+
+            }
+        }
+    }
+}
